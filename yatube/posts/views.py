065749fb22882687ab70039de02d1f68176d8c1d
Александр Маценко <smac4343@gmail.com>
2021-06-13 22:10:34 +0300

    add some notices from reviwer

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 
-
 from .models import Post, Group
 
 
 def index(request):
-    posts = Post.objects.select_related('author') \
-                .select_related('group')
+    posts = Post.objects.all()[:10]
     return render(request, 'index.html', {"posts": posts})
 
 
-def group_posts(self, slug):
+def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
-    return render(Group, "group.html", {"group": group, "posts": posts})
+    posts = group.posts.all()[:10]
+    return render(request, "group.html", {"group": group, "posts": posts})
